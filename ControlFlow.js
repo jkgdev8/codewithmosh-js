6728fee6cfd0acede82900a36a4b1d86baa149cd
jkgdev8 <jkgdev8@gmail.com>
2022-04-18 19:54:48 -0700

    object oriented programming

--- ControlFlow.js
+++ ControlFlow.js
@@ -300,24 +300,24 @@
 // 11 = 1, 11
 // 13 = 1, 13
 
-showPrimes(10);
+// showPrimes(10);
 
-function showPrimes(limit) {
-    for (let number = 2; number <= limit; number++)
+// function showPrimes(limit) {
+//     for (let number = 2; number <= limit; number++)
         
-    if (isPrime(number)) console.log(number);
+//     if (isPrime(number)) console.log(number);
     
-}
+// }
 
-function isPrime(number){
+// function isPrime(number){
    
-    for (let factor = 2; factor < number; factor ++) 
-        if (number % factor === 0) 
+//     for (let factor = 2; factor < number; factor ++) 
+//         if (number % factor === 0) 
            
-        return false;
+//         return false;
          
-    return true;
-}
+//     return true;
+// }
 
 
 
---
+++ index.js
@@ -0,0 +1,18 @@
+// Object-oriented Programming(OOP)
+const circle = {
+    // Properties
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+
+
+    // If a function is part of an object it is called a draw 'Method'
+    draw: function(){
+        console.log('draw');
+    }
+};
+// circle.draw(); // Method
+
