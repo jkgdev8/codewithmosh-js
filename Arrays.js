5bea24560cf1427361cabf06d2e6a9f2a8dc1256
jkgdev8 <jkgdev8@gmail.com>
2022-04-28 20:56:13 -0700

    practice

---
+++ Arrays.js
@@ -0,0 +1,609 @@
+// Object-oriented Programming(OOP)
+// const circle = {
+//     // Properties
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+
+
+//     // If a function is part of an object it is called a draw 'Method'
+//     draw: function(){
+//         console.log('draw');
+//     }
+// };
+// circle.draw(); // Method
+
+
+
+
+// // Factory Function; uses Camel Notation; uses return
+// function createCircle(radius) {
+//     return {
+//         // Properties
+//         // Key: radius, Value: radius. In modern js if the key and value the same just use the word
+//         radius: radius,
+//         // If a function is part of an object it is called a draw 'Method'
+//         draw() {
+//             console.log('draw');
+//         }
+//     };
+// }
+// const circle1 = createCircle(1);
+// console.log(circle1);
+
+// const circle2 = createCircle(2);
+// console.log(circle2);
+
+// // // function draw() {}
+// // // is the same as draw() {  } especially if its a function within a function}
+
+
+// // Camel Notation: oneTwoThreeFour
+// // Pascal Notation: OneTwoThree
+
+
+// Constructor Function; construct or creates an object. uses Pascal Notation, use the this keyword
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+// // the new operator creates a new empty object, it basically returns it
+// const circle = new Circle(1);
+// console.log(circle.draw());
+
+
+// My own factory function practice
+// function circle(radius) {
+//     return{
+//         radius: radius,
+//         draw(){
+//             console.log('Practice Makes Perfect Bro!');
+//         }
+//     }
+// }
+// const circle1 = circle(5);
+// console.log(circle1);
+// console.log(circle1.draw());
+
+
+
+
+
+// Dynamic Nature of Objects
+// const circle = {
+//     // Property
+//     radius: 1
+// };
+// // you can dynamically add properties as well as methods(functions in an object)
+// circle.color = "yellow";
+// // Method
+// circle.draw = function(){
+// };
+
+// // you can also delete properties with the delete operator
+// // delete circle.color;
+// // delete circle.draw;
+
+// console.log(circle);
+
+
+
+
+
+
+// Constructor Property
+// TAKEAWAY: Every object has a constructor property and references the function that was used to create that object
+
+// Factory Function
+// function createCircle(radius) {
+//     return {
+        
+//         radius: radius,
+        
+//         draw() {
+//             console.log('draw');
+//         }
+//     };
+// }
+// const circle = createCircle(1);
+
+// // Constructor Function
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const another = new Circle(1);
+
+
+
+
+
+
+
+
+
+
+// // Functions are Objects... function are basically objects
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// // Blues are properties, and purples are methods
+// Circle.apply;
+// Circle.arguments;
+
+// // The argument empty object will refer to 'this' new const
+// Circle.call({},1);
+
+// // With multiple arguments, you pass them in as an array
+// Circle.apply({}, [1, 2, 3, 4] )
+
+
+// const another = new Circle(1);
+
+
+
+
+
+
+
+
+
+
+// Value vs Reference Types
+// Primitievs are copied by their value
+// Objects are copied by their reference
+
+// let x = 10;
+// let y = x;
+// x = 20;
+
+// let x = {  //properties
+//             value: 10
+// };
+// // y will equal 10 due to, the object is not primarily stored in the variable, its stored somewhere else in memory
+// let y = x;
+// x.value = 20;
+
+// objects are copied by their reference
+// let obj = { value: 10 };
+// function increase(obj){
+//     obj.value++;
+
+// }
+
+// increase(obj);
+// console.log(obj);
+
+
+
+
+
+
+// // Enumerating Properties of an Object
+
+// const circle = {
+//     radius: 1,
+//     draw() {
+//         console.log('draw'); 
+//     }
+// };
+
+// // Simplest way to enumerate the properties in an object for in
+// for (let key in circle)
+// console.log(key, circle[key]);
+
+
+// // for of loop can only be used by iterables like arrays and maps, this will throw an error
+// // for (let key of circle)
+// // console.log(key);
+ 
+
+
+
+
+
+
+
+
+
+
+// // Cloning an Object
+// const circle = {
+//     radius: 1,
+//     draw() {
+//         console.log('draw'); 
+//     }
+// };
+
+// // clonding an object to a new empty object
+// // const another = {};
+// // for (let key in circle)
+// // another[key] = circle[key];
+
+
+// // This version is much cleaner and updated, the circle object will be cloned into a empty object by assigning
+// // You can also add properties within the cloned object
+// // const another = Object.assign({
+// //     color: 'red'
+// // }, circle)
+// // const another = Object.assign({}, circle)
+
+// // Cleanest and most simplest way to clone an object is the ...Spread operator, as well as you can add more properties
+// const another = { ...circle, 
+//     // color: 'red'
+// }
+
+// console.log(another);
+
+
+
+
+
+
+
+// Garbage collection
+// Javascript engine has a garbage collector, its job is to find the variables and consts that are no longer in use and then dealocate the memory that wasw allocated to them earlier
+// let circle = {};
+// console.log(circle);
+
+
+
+
+
+
+
+
+
+// Built in Objects: MATH
+// Mozilla has alot of properties and methods used for math constants and functions
+// ex 
+// function getRandomNum (min, max) {
+//     return Math.random() * (max-min) +min;
+// }
+
+// getRandomNum();
+
+// const number1 = getRandomNum(1, 10);
+
+// console.log(number1);
+
+
+
+
+
+
+
+
+
+
+
+// // Built In Objects: STRING(Primitive Type)
+
+// // String Primitive
+// const message = " This is my\n first message ";
+
+// // Index example it will show 'T'
+// message[0];
+
+// // Index example it will shot 'h'
+// message[0];
+
+// // includes example will result in true or false(METHOD), its also case sensitive
+// message.includes('my')
+
+
+// // Property example is length
+// message.length
+
+// // Method example
+// message.concat()
+
+
+// // String Object
+// const another = new String('Hi');
+
+// // Trim method
+// message.trim()
+
+// // Escape character, use the \ backslash to put quotes in a string
+// // new line for strings are \n
+
+
+// // Split is also popular
+
+
+
+
+
+
+
+
+// Template Litterals ``
+// const message = "This is my\n first message";
+
+
+// // Template literals you can also use placeholders such as dynamic variables with the ${}, these are called expressions
+// const name ='Julienne'
+// const another = 
+// `This is ${name} my
+// 'first' template message
+// how are you?
+// cool`;
+
+
+// console.log(message);
+// console.log(another);
+// // Best way is template literals when doing your own format, also its cleaner
+
+
+
+
+
+
+// Built in Object: DATE
+// Date() is a constructor function it will give you a preview of parameters
+// const now = new Date();
+// const date1 = new Date('May 11 2018 09:00');
+// const date2 = new Date(2022, 3, 25, 9);
+// console.log(date2);
+
+// // All these date objects have alot of get and set methods
+// now.getTime;
+// now.setDate;
+// now.getFullYear(2022);
+
+
+
+// Exercise 1: Address Object
+// street, city, zip
+// My example...
+// const address = {
+//     street: "Jordan Ave",
+//     city: "Canoga Park",
+//     zipCode: 91303
+// };
+// function showAddress(address){
+//     return address;
+
+// }
+
+// // console.log(showAddress(address.street));
+// // console.log(showAddress(address.city));
+// // console.log(showAddress(address.zipCode));
+// console.log(address);
+
+// Mosh Example
+// let address = {
+//     street: 'Jordan Ave',
+//     city: 'Canoga Park',
+//     zipCode: 91303
+// };
+
+// function showAddress(address){
+//     for (let key in address)
+//     console.log(key, address[key]);
+// }
+
+// showAddress(address);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Factory and Constructor Functions
+// My Factory Function
+// function showAddress(street, city, zipCode){
+//     return{
+//         street: street,
+//         city: city,
+//         zipCode: zipCode
+//     }
+// }
+
+// const show = showAddress('Jordan Ave', 'Canoga Park', 91303);
+// console.log(show.street);
+// console.log(show.city);
+// console.log(show.zipCode);
+
+
+// My Constructor Function
+// function Showaddress(street, city, zipCode){
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode
+// }
+
+// const show = new Showaddress('a', 'b', 91303);
+// console.log(show.street);
+// console.log(show.city);
+// console.log(show.zipCode);
+
+
+
+// Mosh Factory Function Example
+// let address = {
+//     street: 'Jordan Ave',
+//     city: 'Canoga Park',
+//     zipCode: 91303
+// }
+// let address = createAddress('Jordan Ave', 'Canoga Park', 91303)
+
+// function createAddress(street, city, zipCode){
+//     return{
+//         street: street,
+//         city: city,
+//         zipCode: zipCode
+//     };
+
+// }
+// console.log(address);
+
+
+// Mosh Constructor Function Example
+// let address = new Createaddress('Jordan Ave', 'Canoga Park', 91303)
+
+// function Createaddress(street, city, zipCode){
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode
+// }
+
+// console.log(address);
+
+
+
+
+
+
+
+
+// Exercise 3: Object Equality
+// Objects are copied by their reference rather than value
+// let address1 = new Createaddress('Jordan Ave', 'Canoga Park', 91303);
+// let address2 = new Createaddress('Jordan Ave', 'Canoga Park', 91303);
+
+// function Createaddress(street, city, zipCode){
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode
+// }
+
+
+// function areEqual(address1, address2){
+//    return address1.street === address2.street &&
+//     address1.city === address2.city &&
+//     address1.zipCode === address2.zipCode;
+   
+   
+   
+// };
+
+// function areSame(address1, address2){
+//     return address1 === address2
+
+// };
+
+// console.log(areEqual(address1, address2));
+// console.log(areSame(address1, address2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Exercise 4 Blog Post Object
+// const blogPost = {
+//     title: `Star Wars`,
+//     body: `Episode 1`,
+//     author: `George Lucas`,
+//     views: 1000,
+//     comments: [
+//         { author: `Lebron`, body: `Whats up`},
+//         { author: `Anthony`, body: `YO`}
+//     ],
+//     isLive: true
+// };
+
+// console.log(blogPost);
+
+
+
+
+
+
+
+
+// Exercise 5 - Constructor Functions
+// const blogPost = {
+//     title: `Star Wars`,
+//     body: `Episode 1`,
+//     author: `George Lucas`,
+//     views: 1000,
+//     comments: [
+//         { author: `Lebron`, body: `Whats up`},
+//         { author: `Anthony`, body: `YO`}
+//     ],
+//     isLive: true
+// }
+
+// let post = new Post('a', 'b', 'c')
+
+
+// function Post(title, body, author){
+//     this.title = title,
+//     this.body =  body,
+//     this.author = author,
+//     this.views = 0,
+//     this.comments = [];
+//     this.isLive = false
+
+
+// }
+
+// console.log(post);
+
+
+
+
+
+
+
+
+
+
+// Exercise 6 Price Range Object
+// let priceRanges = [
+//     { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+//     { label: '$$', tooltip: 'Moderate', minPerPerson: 10, maxPerPerson: 20 },
+//     { label: '$$$', tooltip: 'Expensive', minPerPerson: 20, maxPerPerson: 30 },
+// ];
+
+// let restaurants = [
+//     {averageperPerson: 5}
+// ];
+
+// console.log(priceRanges);
+// console.log(restaurants);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
