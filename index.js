48f5a70e1d68343037a9f73fbdc276d17a59c909
jkgdev8 <jkgdev8@gmail.com>
2022-04-29 22:56:10 -0700

    arrow funciton

--- index.js
+++ index.js
@@ -40,21 +40,61 @@
 
 
 // Finding Elements in an array (Reference Types)
-const courses =[
-    {id: 1, name: 'a'},
-    {id: 2, name: 'b'},
-];
+// const courses =[
+//     {id: 1, name: 'a'},
+//     {id: 2, name: 'b'},
+// ];
 
 
-// These are 2 different obejcts, hence why it console logs false
-// console.log(courses.includes({id: 1, name: 'a'}));
-const course = courses.find(function(course) {
-    return course.name === 'x';
-});
+// // These are 2 different obejcts, hence why it console logs false
+// // console.log(courses.includes({id: 1, name: 'a'}));
+// const course = courses.find(function(course) {
+//     return course.name === 's';
+// });
 
-console.log(course);
+// // It will show up as undefined because there is no 's' in the object
+
+// console.log(course);
 
 
+
+
+
+
+
+
+
+// Arrow Functions
+// You can use arrow functions to pass a function as an argument to a method
+// const courses =[
+//     {id: 1, name: 'a'},
+//     {id: 2, name: 'b'},
+// ];
+// // if only 1 parameter you dont need parethesis, if none, use empty parenthesis
+// const course = courses.find(course => course.name === 'a');
+// console.log(course);
+
+
+
+
+// Regular function vs Arrow function es6 2015
+// function greet(who) {
+//     return `Hi, ${who}`
+// }
+
+// console.log(greet('Julienne'));
+
+// const greeting = who => {
+//     return `Hi, ${who}`
+// }
+
+// console.log(greeting('Garcia'));
+
+const greet = (who, when, what) => {
+    return `Hi, ${who} ${when} ${what}`
+}
+
+console.log(greet('Julienne', 'Kristopher', 'Garcia'));
 
 
 
