ccb63012b817d52c2ad6f70986a821a044a3b8ab
jkgdev8 <jkgdev8@gmail.com>
2022-05-07 00:35:56 -0700

    practice

--- index.js
+++ index.js
@@ -467,7 +467,7 @@
 //      return output;
 // };
 
-// my parctice
+// my practice
 // const numbers = [1, 2, 3, 4];
 // function except(array, excludes){
 //     const output = [];
@@ -478,6 +478,36 @@
 // }
 // const output = except(numbers, [1]);
 // console.log(output);
+
+
+
+
+
+
+
+
+
+// excercise 4 - moving an element
+const numbers = [1, 2, 3, 4];
+
+const output = move(numbers, 0, 1);
+
+console.log(output);
+
+
+function move(array, index, offset){
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(index + offset, 0, element);
+    return output
+    
+}
+
+
+
+
+
+
 
 
 
