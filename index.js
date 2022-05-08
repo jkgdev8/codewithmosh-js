c5c84c9f387c4cf50073e55a9df2f9fac1389c65
jkgdev8 <jkgdev8@gmail.com>
2022-05-07 23:58:27 -0700

    moving and element

--- index.js
+++ index.js
@@ -488,20 +488,20 @@
 
 
 // excercise 4 - moving an element
-const numbers = [1, 2, 3, 4];
+// const numbers = [1, 2, 3, 4];
 
-const output = move(numbers, 0, 1);
+// const output = move(numbers, 0, 1);
 
-console.log(output);
+// console.log(output);
 
 
-function move(array, index, offset){
-    const output = [...array];
-    const element = output.splice(index, 1)[0];
-    output.splice(index + offset, 0, element);
-    return output
+// function move(array, index, offset){
+//     const output = [...array];
+//     const element = output.splice(index, 1)[0];
+//     output.splice(index + offset, 0, element);
+//     return output
     
-}
+// }
 
 
 
