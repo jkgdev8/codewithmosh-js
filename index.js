4b30e73513b83c87c4ceaefd4aa7cf33afa5f71f
jkgdev8 <jkgdev8@gmail.com>
2022-05-09 00:54:26 -0700

    movies exercise

--- index.js
+++ index.js
@@ -601,6 +601,48 @@
 
 
 
+// Excercise 7 - Movies
+const movies = [
+    {title: 'a', year: 2018, rating: 4.5},
+    {title: 'b', year: 2018, rating: 4.7},
+    {title: 'c', year: 2018, rating: 3},
+    {title: 'd', year: 2017, rating: 4.5},
+
+    // All movies in 2018 with rating > 4
+    // sort them by their rating
+    // descending order
+    // pick their title
+
+]
+// // my method
+// const filtered = movies
+// .filter(movies => movies.rating >= 4)
+// .filter(movies => movies.year >= 2018)
+
+// console.log(filtered);
+
+
+// Mosh Method
+const filtered = movies
+.filter(m => m.year === 2018 && m.rating >= 4)
+.sort((a, b) => a.rating - b.rating)
+.reverse()
+.map(m => m.title)
+
+console.log(filtered);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
