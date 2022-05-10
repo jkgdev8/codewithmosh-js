a46bd10291468536e3f72eeefe7d40170f7f5429
jkgdev8 <jkgdev8@gmail.com>
2022-05-09 20:42:39 -0700

    rest operator

--- index.js
+++ index.js
@@ -18,3 +18,72 @@
 // let move = run;
 // run();
 // move();
+
+
+
+
+
+
+
+
+// // Hoisting - process of moving function declarations to the top of the file
+
+// // In run time functions will execute first and put it on top
+// walk();
+// function walk() {
+//     console.log('walk');
+// }
+
+// // Function Expression
+// // will cause an error due to the function is called first before variable
+// //run();
+// const run = function(){
+//     console.log('run');
+// };
+
+
+
+
+
+
+// // Arguments
+// // function sum (a, b) {
+// //     console.log(arguments);
+// //     return a + b;
+// // }
+
+// // console.log(sum(1, 2, 3, 4, 5));
+
+// // Iterate arguments values
+// function sum () {
+//     let total = 0;
+//     for (let value of arguments)
+//      total += value;
+//     return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+
+
+
+
+
+
+// Rest operator(DONT get confused for spread... operator, this is only for parameters)
+
+// Rest parameter syntax allows a function to accept an indefinite number of arguments as an array, providing a way to represent
+// Spread syntax (...) allows an iterable such as an array expression or string to be expanded in places where zero or more arguments
+// function sum (...args) {
+//     return args.reduce((a, b) => a + b);
+// }
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum (discount, ...prices) {
+    const total = prices.reduce((a, b) => a + b);
+    return total * (1 - discount);
+}
+
+console.log(sum(0.1, 20, 30, 1));
+
+
